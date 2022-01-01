f0133bad2363021654de041be4aafda037994ccd
Bashar Louzon <bashar.louzon@gmail.com>
2022-01-01 23:37:42 +0200

    Temp App

---
+++ lib/main.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() => runApp(MaterialApp(
+      title: "Weather App",
+      home: Home(),
+    ));
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var temp;
+  var description;
+  var currently;
+  var humidity;
+  var windSpeed;
+  var cityName;
+  var feels_like;
+
+  Future getWeather() async {
+    http.Response response = await http.get(Uri.parse(
+        "https://api.openweathermap.org/data/2.5/weather?q=Gaza&units=metric&appid=81afede4c879aac15efed5bf2cd47b8b"));
+    var results = jsonDecode(response.body);
+    setState(() {
+      this.temp = results['main']['temp'];
+      this.description = results['weather'][0]['description'];
+      this.feels_like = results['main']['feels_like'];
+      this.currently = results['weather'][0]['main'];
+      this.humidity = results['main']['humidity'];
+      this.windSpeed = results['wind']['speed'];
+      this.cityName = results['name'];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.getWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height / 3,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.red,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10.0),
+                  child: Text(
+                    cityName != null ? cityName.toString() : "loading",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  temp != null ? temp.toString() : "loading",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Text(
+                    currently != null ? currently.toString() : "loading",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(20.0),
+              child: ListView(
+                children: [
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.temperatureHigh),
+                    title: Text("Feels Like"),
+                    trailing: Text(feels_like != null
+                        ? feels_like.toString() + "\u00B0"
+                        : "loading "),
+                  ),
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.cloud),
+                    title: Text("Weather"),
+                    trailing: Text(description != null
+                        ? description.toString()
+                        : "loading"),
+                  ),
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.sun),
+                    title: Text("Humidity"),
+                    trailing: Text(
+                        humidity != null ? humidity.toString() : "loeading"),
+                  ),
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.wind),
+                    title: Text("Wind Speed"),
+                    trailing: Text(
+                        windSpeed != null ? windSpeed.toString() : "loading"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
